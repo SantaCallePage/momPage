a1b9a62c48986ab34936d94ed4431923d4c89c97
Luca <stomluca@gmail.com>
2026-08-03 10:36:05 -0300

    Lo mismo que el commit anterior

--- vercel.ts
+++ vercel.ts
@@ -2,7 +2,6 @@ import type { VercelConfig } from '@vercel/config/v1'
 
 export const config: VercelConfig = {
   images: {
-    
     sizes: [100, 200, 256, 500, 640, 1080, 2048, 3840],
     qualities: [25, 50, 75],
     localPatterns: [
@@ -14,7 +13,7 @@ export const config: VercelConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.SUPABASE_HOSTNAME,
+        hostname: process.env.SUPABASE_HOSTNAME || "bodvkovfmzgqeqqzxiwr.supabase.co",
         port: '',
         pathname: '^/storage/v1/object/public/.*$',
       },
