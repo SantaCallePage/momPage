1ef16219d9b722f2af41cf55611dfe999817ded8
Luca <stomluca@gmail.com>
2026-08-03 10:31:23 -0300

    Este commit es only para que Vercel pueda hacer un redeploy (idk por qué no me deja redeployear)

--- vercel.ts
+++ vercel.ts
@@ -2,6 +2,7 @@ import type { VercelConfig } from '@vercel/config/v1'
 
 export const config: VercelConfig = {
   images: {
+    
     sizes: [100, 200, 256, 500, 640, 1080, 2048, 3840],
     qualities: [25, 50, 75],
     localPatterns: [
